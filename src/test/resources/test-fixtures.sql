59686c840b4837a7056dcc22e5beca63536e2061
Arnaud Deman <arnaud.deman@univ-tln.fr>
2024-07-12 17:33:27 +0200

    fix: invalid scope names

--- src/test/resources/test-fixtures.sql
+++ src/test/resources/test-fixtures.sql
@@ -85,19 +85,19 @@ VALUES
 
 INSERT INTO scope (id, name)
 VALUES 
-(1, 'scope_00000'),
-(2, 'scope_00001'),
-(3, 'scope_00002'),
-(4, 'scope_00003');
+(1, 'scope_0000'),
+(2, 'scope_0001'),
+(3, 'scope_0002'),
+(4, 'scope_0003');
 
 
 INSERT INTO scope_resource (id_scope, id_resource)
 VALUES 
-((SELECT id FROM scope WHERE name = 'scope_00000'), (SELECT id FROM resource WHERE selector = 'ptf_0000')),
-((SELECT id FROM scope WHERE name = 'scope_00001'), (SELECT id FROM resource WHERE selector = 'ptf_0001')),
-((SELECT id FROM scope WHERE name = 'scope_00002'), (SELECT id FROM resource WHERE selector = 'sae_0000')),
-((SELECT id FROM scope WHERE name = 'scope_00003'), (SELECT id FROM resource WHERE selector = 'ptf_0000')),
-((SELECT id FROM scope WHERE name = 'scope_00003'), (SELECT id FROM resource WHERE selector = 'ptf_0001'));
+((SELECT id FROM scope WHERE name = 'scope_0000'), (SELECT id FROM resource WHERE selector = 'ptf_0000')),
+((SELECT id FROM scope WHERE name = 'scope_0001'), (SELECT id FROM resource WHERE selector = 'ptf_0001')),
+((SELECT id FROM scope WHERE name = 'scope_0002'), (SELECT id FROM resource WHERE selector = 'sae_0000')),
+((SELECT id FROM scope WHERE name = 'scope_0003'), (SELECT id FROM resource WHERE selector = 'ptf_0000')),
+((SELECT id FROM scope WHERE name = 'scope_0003'), (SELECT id FROM resource WHERE selector = 'ptf_0001'));
 
 
 INSERT INTO principal (login)
@@ -114,10 +114,10 @@ VALUES ('empty', 'Default empty context');
 
 INSERT INTO assignment (id_role, id_principal, id_scope, id_context)
 VALUES 
-((SELECT id FROM role WHERE name = 'ROLE_OWNER'), (SELECT id FROM principal WHERE login = 'deman'), (SELECT id FROM scope WHERE name = 'scope_00000'), (SELECT id FROM context WHERE name = 'empty')),
-((SELECT id FROM role WHERE name = 'ROLE_PAIR'), (SELECT id FROM principal WHERE login = 'deman'), (SELECT id FROM scope WHERE name = 'scope_00002'), (SELECT id FROM context WHERE name = 'empty')),
-((SELECT id FROM role WHERE name = 'ROLE_OWNER'), (SELECT id FROM principal WHERE login = 'gribonvald'), (SELECT id FROM scope WHERE name = 'scope_00001'), (SELECT id FROM context WHERE name = 'empty')),
-((SELECT id FROM role WHERE name = 'ROLE_PAIR'), (SELECT id FROM principal WHERE login = 'dugat'), (SELECT id FROM scope WHERE name = 'scope_00002'), (SELECT id FROM context WHERE name = 'empty'));
+((SELECT id FROM role WHERE name = 'ROLE_OWNER'), (SELECT id FROM principal WHERE login = 'deman'), (SELECT id FROM scope WHERE name = 'scope_0000'), (SELECT id FROM context WHERE name = 'empty')),
+((SELECT id FROM role WHERE name = 'ROLE_PAIR'), (SELECT id FROM principal WHERE login = 'deman'), (SELECT id FROM scope WHERE name = 'scope_0002'), (SELECT id FROM context WHERE name = 'empty')),
+((SELECT id FROM role WHERE name = 'ROLE_OWNER'), (SELECT id FROM principal WHERE login = 'gribonvald'), (SELECT id FROM scope WHERE name = 'scope_0001'), (SELECT id FROM context WHERE name = 'empty')),
+((SELECT id FROM role WHERE name = 'ROLE_PAIR'), (SELECT id FROM principal WHERE login = 'dugat'), (SELECT id FROM scope WHERE name = 'scope_0002'), (SELECT id FROM context WHERE name = 'empty'));
 
 
 
