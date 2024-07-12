ad2b4597066dd0643bb76734c27984e91c9e5498
Arnaud Deman <arnaud.deman@univ-tln.fr>
2024-07-12 17:20:28 +0200

    fix: imports

--- src/test/java/fr/avenirsesr/portfolio/security/services/RBACAssignmentServiceTest.java
+++ src/test/java/fr/avenirsesr/portfolio/security/services/RBACAssignmentServiceTest.java
@@ -2,11 +2,9 @@ package fr.avenirsesr.portfolio.security.services;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
-import java.util.ArrayList;
 import java.util.List;
 
 import org.junit.jupiter.api.Test;
-import org.mockito.internal.creation.SuspendMethod;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.ActiveProfiles;
