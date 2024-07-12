b6a57465fa907a2874e0227633c06d7d32554ac8
Arnaud Deman <arnaud.deman@univ-tln.fr>
2024-07-12 17:44:25 +0200

    test: scope service

--- src/test/java/fr/avenirsesr/portfolio/security/services/RBACScopeServiceTest.java
+++ src/test/java/fr/avenirsesr/portfolio/security/services/RBACScopeServiceTest.java
@@ -1,5 +1,7 @@
 package fr.avenirsesr.portfolio.security.services;
 
+import static org.assertj.core.api.Assertions.assertThat;
+
 import java.util.List;
 
 import org.junit.jupiter.api.Test;
@@ -17,13 +19,50 @@ import jakarta.transaction.Transactional;
 @Sql(scripts = "classpath:test-fixtures.sql")
 @Transactional
 class RBACScopeServiceTest {
+	
+	private static final Long RES1_ID=1L;
+	private static final Long RES2_ID=2L;
+	private static final Long RES3_ID=3L;
+	private static final Long RES4_ID=4L;
+	
+	private final static String[] EXPECTED_SCOPES_RES1 = {"scope_0000", "scope_0003"};
+	private final static String[] EXPECTED_SCOPES_RES3 = {"scope_0002"};
+	private final static String[] EXPECTED_SCOPES_RES4 = {};
+	private final static String[] EXPECTED_SCOPES_RES1_RES3 = {"scope_0000",  "scope_0002", "scope_0003"};
+	private final static String[] EXPECTED_SCOPES_RES_ALL = {"scope_0000",  "scope_0001",  "scope_0002", "scope_0003"};
+	
+	
+	
 	@Autowired 
 	private RBACScopeService scopeService;
 	
 	@Test
 	void testGetAllScopesByPredicate() {
-		List<RBACScope> scopes = scopeService.getAllScopesByPredicate(RBACScopeSpecification.filterByResources((long)1, (long)2));
-		System.out.println("scopes: " + scopes);
+		List<RBACScope> actual = scopeService.getAllScopesByPredicate(RBACScopeSpecification.filterByResources(RES1_ID));
+		assertThat(actual).hasSize(EXPECTED_SCOPES_RES1.length);
+		assertThat(actual.stream().map(s->s.getName())).contains(EXPECTED_SCOPES_RES1);
+		
+		actual = scopeService.getAllScopesByPredicate(RBACScopeSpecification.filterByResources(RES3_ID));
+		assertThat(actual).hasSize(EXPECTED_SCOPES_RES3.length);
+		assertThat(actual.stream().map(s->s.getName())).contains(EXPECTED_SCOPES_RES3);
+		
+		actual = scopeService.getAllScopesByPredicate(RBACScopeSpecification.filterByResources(RES4_ID));
+		assertThat(actual).isEmpty();
+		
+		
+		actual = scopeService.getAllScopesByPredicate(RBACScopeSpecification.filterByResources(RES1_ID, RES3_ID));
+		assertThat(actual).hasSize(EXPECTED_SCOPES_RES1_RES3.length);
+		assertThat(actual.stream().map(s->s.getName())).contains(EXPECTED_SCOPES_RES1_RES3);
+		
+		// Res4 is not referenced by any scope, so adding it id should not change the result.
+		actual = scopeService.getAllScopesByPredicate(RBACScopeSpecification.filterByResources(RES1_ID, RES4_ID));
+		assertThat(actual).hasSize(EXPECTED_SCOPES_RES1.length);
+		assertThat(actual.stream().map(s->s.getName())).contains(EXPECTED_SCOPES_RES1);
+		
+		actual = scopeService.getAllScopesByPredicate(RBACScopeSpecification.filterByResources(RES1_ID, RES2_ID, RES3_ID,RES4_ID));
+		assertThat(actual).hasSize(EXPECTED_SCOPES_RES_ALL.length);
+		assertThat(actual.stream().map(s->s.getName())).contains(EXPECTED_SCOPES_RES_ALL);
+		
 	}
 
 }
