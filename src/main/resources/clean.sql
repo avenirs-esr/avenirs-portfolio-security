21da1af3db47a360dc7e77e00ca5ef2bfbfa1d2e
Arnaud Deman <arnaud.deman@univ-tln.fr>
2024-07-16 15:46:12 +0200

    fix: typos and missing drop db for tests

--- src/main/resources/clean.sql
+++ src/main/resources/clean.sql
@@ -1,4 +1,6 @@
 \connect template1;
 
-DROP DATABASE IF EXISTS avenirs_access_controlDROP ROLE IF EXISTS avenirs_security_admin;
+DROP DATABASE IF EXISTS avenirs_access_control;
+DROP DATABASE IF EXISTS avenirs_access_control_test;
+DROP ROLE IF EXISTS avenirs_security_admin;
 DROP ROLE IF EXISTS avenirs_security_admin_role;
