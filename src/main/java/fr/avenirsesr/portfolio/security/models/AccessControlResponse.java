5e3f3f22795d2294647985a9a8a881bfd3fc099b
Arnaud <arnaud.deman@univ-tln.fr>
2024-07-17 16:27:51 +0200

    feat: access control response model

---
+++ src/main/java/fr/avenirsesr/portfolio/security/models/AccessControlResponse.java
@@ -0,0 +1,44 @@
+/**
+ * 
+ */
+package fr.avenirsesr.portfolio.security.models;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.experimental.Accessors;
+
+
+/**
+ * Access control response
+ */
+@Data()
+@Accessors(chain=true)
+@NoArgsConstructor
+public class AccessControlResponse {
+	
+	/** Access token. */
+	private String token;
+	
+	
+	/** Login of the user. */
+	private String login;
+	
+	
+	/** Name of the action. */
+	private String actionName;
+	
+	/** Id of the resource. */
+	private Long resourceId;
+	
+	/** Access granted or not. */
+	private boolean granted;
+	
+	/** Uri */
+	private String uri;
+	
+	/** HTTP method. */
+	private String method;
+	
+		
+}
