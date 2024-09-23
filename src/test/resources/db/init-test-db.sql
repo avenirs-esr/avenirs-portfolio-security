ac9dbfa1580bacd4bb93d5470c9a0fd39520248f
Arnaud Deman <arnaud.deman@univ-tln.fr>
2024-09-23 16:29:57 +0200

    fix: invalid grant

--- src/test/resources/db/init-test-db.sql
+++ src/test/resources/db/init-test-db.sql
@@ -1,6 +1,6 @@
 CREATE ROLE avenirs_security_admin_role_test SUPERUSER; 
 CREATE ROLE avenirs_security_admin_test PASSWORD 'S33c@DM4avn' NOSUPERUSER CREATEDB CREATEROLE INHERIT LOGIN;
-GRANT avenirs_security_admin_role_test to avenirs_security_admin;
+GRANT avenirs_security_admin_role_test to avenirs_security_admin_test;
 
 CREATE DATABASE avenirs_access_control_test OWNER avenirs_security_admin_test;
 GRANT ALL PRIVILEGES ON DATABASE avenirs_access_control_test TO avenirs_security_admin_role_test;
