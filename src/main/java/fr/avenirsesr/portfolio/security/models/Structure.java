d4bc1e8aa918f47ab2026e0555c53ac4c22115f4
Arnaud Deman <arnaud.deman@univ-tln.fr>
2024-10-01 15:15:30 +0200

    feat: structure model

---
+++ src/main/java/fr/avenirsesr/portfolio/security/models/Structure.java
@@ -0,0 +1,50 @@
+/**
+ * 
+ */
+package fr.avenirsesr.portfolio.security.models;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+
+/**
+ * 
+ * <h1>Structure</h1>
+ * <p>
+ * Description:  is used to represent the notion of structure.
+ * </p>
+ * 
+ * <h2>Version:</h2>
+ * 1.0
+ * 
+ * <h2>Author:</h2>
+ * Arnaud Deman
+ *
+ * <h2>Since:</h2>
+ * 1 Oct 2024
+ */
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+@Table(name="structure")
+public class Structure {
+	
+	/** Database Id. */
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private long id;
+	
+	/** Name of the structure. */
+	private String name;
+	
+	/** The description of the structure. */
+	private String description;
+	
+}
