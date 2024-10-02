0e296c18d6d1fed8ac24682dd04f089b5262cb40
Arnaud Deman <arnaud.deman@univ-tln.fr>
2024-10-02 17:33:24 +0200

    refactor: removed unecessary constructors

--- src/main/java/fr/avenirsesr/portfolio/security/models/Principal.java
+++ src/main/java/fr/avenirsesr/portfolio/security/models/Principal.java
@@ -3,10 +3,18 @@
  */
 package fr.avenirsesr.portfolio.security.models;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import jakarta.persistence.CascadeType;
 import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.JoinTable;
+import jakarta.persistence.ManyToMany;
 import jakarta.persistence.Table;
 import lombok.AllArgsConstructor;
 import lombok.Data;
@@ -31,13 +39,22 @@ public class Principal {
 	/** Login of the user. */
 	private String login;
 	
-	public Principal(Long id) {
-		this.id = id;
-	}
 	
-	public Principal(String login) {
-		this.login = login;
-	}
+	/** Structures associated to the principal. */
+    @ManyToMany(
+            fetch=FetchType.LAZY,
+            cascade = {
+                    CascadeType.PERSIST,
+                    CascadeType.MERGE
+            }
+    )
+    @JoinTable(
+            name="principal_structure",
+            joinColumns = @JoinColumn(name= "id_principal"),
+            inverseJoinColumns = @JoinColumn(name="id_structure")
+            
+    )
+    private List<Structure> structures = new ArrayList<>();
 	
 		
 }
