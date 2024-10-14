380bab7739c6fab3953a6553b133ae2ada85ce00
Arnaud Deman <arnaud.deman@univ-tln.fr>
2024-10-14 16:53:08 +0200

    refactor: method renamed in the tested service

--- src/test/java/fr/avenirsesr/portfolio/security/services/RBACActionRouteServiceTest.java
+++ src/test/java/fr/avenirsesr/portfolio/security/services/RBACActionRouteServiceTest.java
@@ -32,12 +32,12 @@ class RBACActionRouteServiceTest {
 
 	@Test
 	void testGetActionRouteByilterByURIAndMethodPredicateF() {
-		var route = actionRouteService.getActionRouteByPredicate(RBACActionRouteSpecification.filterByURIAndMethod(ROUTE_1, METH_1)).get();
+		var route = actionRouteService.getAllActionRoutesBySpecification(RBACActionRouteSpecification.filterByURIAndMethod(ROUTE_1, METH_1)).get();
 		assertEquals(ROUTE_1, route.getUri());
 		assertEquals(METH_1, route.getMethod());
 		assertEquals(ACTION_NAME_1, route.getAction().getName());
 		
-		route = actionRouteService.getActionRouteByPredicate(RBACActionRouteSpecification.filterByURIAndMethod(ROUTE_2, METH_2)).get();
+		route = actionRouteService.getAllActionRoutesBySpecification(RBACActionRouteSpecification.filterByURIAndMethod(ROUTE_2, METH_2)).get();
 		assertEquals(ROUTE_2, route.getUri());
 		assertEquals(METH_2, route.getMethod());
 		assertEquals(ACTION_NAME_2, route.getAction().getName());
