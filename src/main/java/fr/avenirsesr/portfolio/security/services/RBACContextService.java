994f271eaf95b82104ced4acab6ab0ed7cb87e23
Arnaud Deman <arnaud.deman@univ-tln.fr>
2024-10-16 15:19:41 +0200

    feat: missing crud method

--- src/main/java/fr/avenirsesr/portfolio/security/services/RBACContextService.java
+++ src/main/java/fr/avenirsesr/portfolio/security/services/RBACContextService.java
@@ -4,7 +4,9 @@
 package fr.avenirsesr.portfolio.security.services;
 
 import java.util.List;
+import java.util.Optional;
 
+import jakarta.persistence.EntityNotFoundException;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -47,7 +49,16 @@ public class RBACContextService {
       log.trace("getAllContexts");
       return this.contextRepository.findAll();
   }
-  
+    /**
+     * Gives a specific context by its id.
+     * @param contextId The id of the context to retrieve.
+     * @return The Optional of context with id contextId.
+     */
+    @Transactional(readOnly = true)
+    public Optional<RBACContext> getContextById(final Long contextId){
+        log.trace("getContextById contextId: {}", contextId);
+        return contextRepository.findById(contextId);
+    }
   /**
    * Creates a context.
    * @param context The context to create.
@@ -58,7 +69,25 @@ public class RBACContextService {
       log.trace("createContext, context: {}", context);
       return this.contextRepository.save(context);
   }
-
+    /**
+     * Updates a context.
+     * @param context The context to update.
+     * @return The updated context.
+     */
+    @Transactional
+    public RBACContext updateContext(RBACContext context) {
+        log.trace("updateContext, context: {}", context);
+        return this.contextRepository.findById(context.getId())
+                .map(storedContext -> {
+                    if (!storedContext.equals(context)) {
+                        storedContext.setValidityStart(context.getValidityStart());
+                        storedContext.setValidityEnd(context.getValidityEnd());
+                        storedContext.setStructures(context.getStructures());
+                    }
+                    return storedContext;
+                })
+                .orElseThrow(() -> new EntityNotFoundException("Entity not found with ID: " + context.getId()));
+    }
   /**
    * Deletes a context by id.
    * @param contextId The id of the context to delete.
