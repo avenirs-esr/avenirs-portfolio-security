b60bf25fcd1c600dfd23aa3f7d270002f30ad341
Arnaud Deman <arnaud.deman@univ-tln.fr>
2024-10-16 18:22:07 +0200

    feat: roles end point

--- src/main/java/fr/avenirsesr/portfolio/security/controllers/RoleController.java
+++ src/main/java/fr/avenirsesr/portfolio/security/controllers/RoleController.java
@@ -1,5 +1,10 @@
 package fr.avenirsesr.portfolio.security.controllers;
 
+import fr.avenirsesr.portfolio.security.models.RBACAssignment;
+import fr.avenirsesr.portfolio.security.models.RBACRole;
+import fr.avenirsesr.portfolio.security.repositories.RBACAssignmentSpecificationHelper;
+import fr.avenirsesr.portfolio.security.services.RBACAssignmentService;
+import fr.avenirsesr.portfolio.security.services.RBACRoleService;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -9,16 +14,24 @@ import org.springframework.web.bind.annotation.RestController;
 import fr.avenirsesr.portfolio.security.models.OIDCIntrospectResponse;
 import fr.avenirsesr.portfolio.security.services.AuthenticationService;
 
+import java.util.Collections;
+import java.util.List;
+import java.util.stream.Collectors;
+
 @Slf4j
 @RestController
 public class RoleController {
 	
 	/** Authentication service used to retrieve the user information.*/
 	@Autowired
-	private AuthenticationService authenticationService; 
+	private AuthenticationService authenticationService;
+
+	/** Assignment service, used to retrieve roles assigned to a principal for instance.*/
+	@Autowired
+	private RBACAssignmentService assignmentService;
 	
-	@GetMapping("${avenirs.access.control.roles}")
-	public String getRoles(@RequestHeader(value="x-authorization") String token) {
+	@GetMapping("/roles")
+	public List<String> getRoles(@RequestHeader(value="x-authorization") String token) {
 		log.trace("getRoles, token: {}", token);
 		
 		OIDCIntrospectResponse introspectResponse = authenticationService.introspectAccessToken(token);
@@ -27,9 +40,16 @@ public class RoleController {
 		if (introspectResponse != null  && introspectResponse .isActive()) {
 			String uid = introspectResponse.getUniqueSecurityName();
 			log.trace("getRoles, uid: {}", uid);
+			List<RBACAssignment> assignments = assignmentService.getAllAssignmentsBySpecification(RBACAssignmentSpecificationHelper.filterByPrincipal(uid));
+
+			List<String> roles = assignments.stream()
+					.map(assignment->assignment.getRole().getName())
+					.toList();
+			log.trace("Role for {}: {}", uid, roles);
+			return roles;
 		}
 		
-		return  "getRoles: " + token;
+		return Collections.emptyList();
 	}
 	
 }
