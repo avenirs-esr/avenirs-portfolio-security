e0e4c71a19ba9db8370129a8a860157e67f4349d
Arnaud Deman <arnaud.deman@univ-tln.fr>
2024-10-17 16:01:32 +0200

    feat: repository for RBACResourceType

---
+++ src/main/java/fr/avenirsesr/portfolio/security/repositories/RBACResourceTypeRepository.java
@@ -0,0 +1,25 @@
+package fr.avenirsesr.portfolio.security.repositories;
+
+import fr.avenirsesr.portfolio.security.models.RBACResource;
+import fr.avenirsesr.portfolio.security.models.RBACResourceType;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+/**
+ * <h1>RBACResourceRepository</h1>
+ * <p>
+ * <b>Description:</b> Repository for RBACResourceType models.
+ * </p>
+ *
+ * <h2>Version:</h2>
+ * 1.0.0
+ *
+ * <h2>Author:</h2>
+ * Arnaud Deman
+ *
+ * <h2>Since:</h2>
+ * 07/10/2024
+ */
+
+public interface RBACResourceTypeRepository extends JpaRepository<RBACResourceType, Long>  {
+
+}
