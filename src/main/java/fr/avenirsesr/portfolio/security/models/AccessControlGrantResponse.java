fc9894cc66e094997cf07646d0e152bca46997c4
Arnaud Deman <arnaud.deman@univ-tln.fr>
2024-10-17 19:11:00 +0200

    feat: model for grant responses

---
+++ src/main/java/fr/avenirsesr/portfolio/security/models/AccessControlGrantResponse.java
@@ -0,0 +1,40 @@
+/**
+ * 
+ */
+package fr.avenirsesr.portfolio.security.models;
+
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.experimental.Accessors;
+
+
+/**
+ * <h1>AccessControlGrantResponse</h1>
+ * <p>
+ * <b>Description:</b> response to a grant or (creation or update) operation.
+ * </p>
+ *
+ * <h2>Version:</h2>
+ * 1.0.0
+ *
+ * <h2>Author:</h2>
+ * Arnaud Deman
+ *
+ * <h2>Since:</h2>
+ * 17/10/2024
+ */
+
+@Data()
+@Accessors(chain=true)
+@NoArgsConstructor
+public class AccessControlGrantResponse {
+	
+	/** Login of the user. */
+	private String login;
+
+	/** Access granted or not. */
+	private boolean granted;
+
+	/** Message if an error occures. */
+	private String error;
+}
