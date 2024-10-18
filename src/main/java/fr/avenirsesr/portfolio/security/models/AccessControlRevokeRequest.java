281feccfe65d4eae4033f42829ee69339250bf14
Arnaud Deman <arnaud.deman@univ-tln.fr>
2024-10-18 09:14:12 +0200

    feat: revoke query model

---
+++ src/main/java/fr/avenirsesr/portfolio/security/models/AccessControlRevokeRequest.java
@@ -0,0 +1,41 @@
+package fr.avenirsesr.portfolio.security.models;
+
+
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.experimental.Accessors;
+
+/**
+ * <h1>AccessControlRevokeRequest</h1>
+ * <p>
+ * Description: used to revoke a granted access.
+ *
+ * </p>
+ *
+ * <h2>Version:</h2>
+ * 1.0.0
+ *
+ * <h2>Author:</h2>
+ * Arnaud Deman
+ *
+ * <h2>Since:</h2>
+ * 17/10/2024
+ */
+@Data
+@Accessors(chain = true)
+@NoArgsConstructor
+public class AccessControlRevokeRequest {
+
+    /** The uid of the principal. */
+    private String uid;
+
+    /** The id of the role to grant. */
+    private Long roleId;
+
+    /** The id of the scope */
+    private Long scopeId;
+
+    /** The id of the context. */
+    private Long contextId;
+
+}
