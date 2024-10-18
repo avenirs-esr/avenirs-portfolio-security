6f9807102b719799a31ec3c965ac595b5121eca4
Arnaud Deman <arnaud.deman@univ-tln.fr>
2024-10-18 09:07:30 +0200

    feat: revoke response model

---
+++ src/main/java/fr/avenirsesr/portfolio/security/models/AccessControlRevokeResponse.java
@@ -0,0 +1,40 @@
+/**
+ * 
+ */
+package fr.avenirsesr.portfolio.security.models;
+
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.experimental.Accessors;
+
+
+/**
+ * <h1>AccessControlRevokeResponse</h1>
+ * <p>
+ * <b>Description:</b> response to a revoke operation.
+ * </p>
+ *
+ * <h2>Version:</h2>
+ * 1.0.0
+ *
+ * <h2>Author:</h2>
+ * Arnaud Deman
+ *
+ * <h2>Since:</h2>
+ * 17/10/2024
+ */
+
+@Data()
+@Accessors(chain=true)
+@NoArgsConstructor
+public class AccessControlRevokeResponse {
+	
+	/** Login of the user. */
+	private String login;
+
+	/** Access granted or not. */
+	private boolean revoke;
+
+	/** Message if an error occurs. */
+	private String error;
+}
