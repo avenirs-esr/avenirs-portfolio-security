76d3545b12bf0ed71f73c04b33c7ac0bdea90fd5
Arnaud <arnaud.deman@univ-tln.fr>
2024-11-04 17:14:10 +0100

    fix: error with existing connections

--- src/test/resources/db/clean-test-db.sql
+++ src/test/resources/db/clean-test-db.sql
@@ -1,5 +1,7 @@
 \connect template1;
-
+SELECT pg_terminate_backend(pid)
+FROM pg_stat_activity
+WHERE datname = 'avenirs_access_control' AND pid <> pg_backend_pid();
 DROP DATABASE IF EXISTS avenirs_access_control_test;
 DROP ROLE IF EXISTS avenirs_security_admin_test;
 DROP ROLE IF EXISTS avenirs_security_admin_role_test;
