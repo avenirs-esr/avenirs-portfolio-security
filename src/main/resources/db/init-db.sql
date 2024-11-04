ef72090684e89e09f79474ec6975577f9f71c6b6
Arnaud <arnaud.deman@univ-tln.fr>
2024-11-04 17:11:34 +0100

    fix: errors with default profile and use of role associated to execution context

--- src/main/resources/db/init-db.sql
+++ src/main/resources/db/init-db.sql
@@ -1,4 +1,4 @@
-CREATE ROLE avenirs_security_admin_role SUPERUSER; 
+CREATE ROLE avenirs_security_admin_role SUPERUSER;
 CREATE ROLE avenirs_security_admin PASSWORD 'S33c@DM4avn' NOSUPERUSER CREATEDB CREATEROLE INHERIT LOGIN;
 GRANT avenirs_security_admin_role to avenirs_security_admin;
 
@@ -6,6 +6,7 @@ CREATE DATABASE avenirs_access_control OWNER avenirs_security_admin;
 GRANT ALL PRIVILEGES ON DATABASE avenirs_access_control TO avenirs_security_admin_role;
 \c avenirs_access_control
 CREATE SCHEMA IF NOT EXISTS dev AUTHORIZATION avenirs_security_admin;
+ALTER USER avenirs_security_admin SET search_path TO dev, public;
 CREATE EXTENSION IF NOT EXISTS citext;
 
 
--- src/test/resources/db/init-test-db.sql
+++ src/test/resources/db/init-test-db.sql
@@ -1,5 +1,5 @@
-CREATE ROLE avenirs_security_admin_role_test SUPERUSER; 
-CREATE ROLE avenirs_security_admin_test PASSWORD 'S33c@DM4avn' NOSUPERUSER CREATEDB CREATEROLE INHERIT LOGIN;
+CREATE ROLE avenirs_security_admin_role_test SUPERUSER;
+CREATE ROLE avenirs_security_admin_test PASSWORD 's4dfHty@DHTpa' NOSUPERUSER CREATEDB CREATEROLE INHERIT LOGIN;
 GRANT avenirs_security_admin_role_test to avenirs_security_admin_test;
 
 CREATE DATABASE avenirs_access_control_test OWNER avenirs_security_admin_test;
@@ -7,4 +7,5 @@ GRANT ALL PRIVILEGES ON DATABASE avenirs_access_control_test TO avenirs_security
 
 \c avenirs_access_control_test
 CREATE SCHEMA IF NOT EXISTS test AUTHORIZATION avenirs_security_admin_test;
+ALTER USER avenirs_security_admin_test SET search_path TO test, public;
 CREATE EXTENSION IF NOT EXISTS citext;
