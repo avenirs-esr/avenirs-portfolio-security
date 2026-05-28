54c0fd832b3014a256b4bee001e6fc55849835db
Nathan <nathan.pignon79@gmail.com>
2026-05-28 15:50:55 +0200

    feat(1683): update auth context response

--- src/main/java/fr/avenirsesr/portfolio/security/authentication/application/adapter/dto/IntrospectResponseDTO.java
+++ src/main/java/fr/avenirsesr/portfolio/security/authentication/application/adapter/dto/IntrospectResponseDTO.java
@@ -1,5 +1,3 @@
 package fr.avenirsesr.portfolio.security.authentication.application.adapter.dto;
 
-import java.util.UUID;
-
-public record IntrospectResponseDTO(boolean active, String uniqueSecurityName, UUID userId) {}
+public record IntrospectResponseDTO(boolean active, String uniqueSecurityName) {}
